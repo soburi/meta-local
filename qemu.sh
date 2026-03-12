222eebc5cb1e136107b0b0edcf28596f58a41e1f
TOKITA Hiroshi <tokita.hiroshi@gmail.com>
2026-03-12 22:32:01 +0900

    Update file paths in qemu.sh for dom0x

--- qemu.sh
+++ qemu.sh
@@ -1,6 +1,6 @@
 set -x
 
-cp /home/crs/dom0/poky/build/tmp/deploy/images/qemuarm64/xen-image-minimal-qemuarm64.rootfs.qemuboot.dtb /tmp/xen.dtb
+cp /home/crs/dom0x/poky/build/tmp/deploy/images/qemuarm64/xen-image-minimal-qemuarm64.rootfs.qemuboot.dtb /tmp/xen.dtb
 fdtput -t s -p /tmp/xen.dtb /chosen xen,xen-bootargs "xsm=flask flask=permissive loglvl=all guest_loglvl=all"
 
 qemu-system-aarch64 \
@@ -11,8 +11,7 @@ qemu-system-aarch64 \
   -serial mon:stdio \
   -netdev user,id=net0,hostfwd=tcp::2222-:22 \
   -device virtio-net-pci,netdev=net0,mac=52:54:00:12:34:56 \
-  -kernel /home/crs/dom0/poky/build/tmp/deploy/images/qemuarm64/xen-qemuarm64 \
+  -kernel /home/crs/dom0x/poky/build/tmp/deploy/images/qemuarm64/xen-qemuarm64 \
   -dtb /tmp/xen.dtb \
-  -device guest-loader,addr=0x45000000,kernel=/home/crs/dom0/poky/build/tmp/deploy/images/qemuarm64/Image,bootargs="console=hvc0 earlyprintk=xen dom0_mem=512M dom0_max_vcpus=2 loglvl=all guest_loglvl=all loglvl_xen=all" \
-  -drive file=/home/crs/dom0/poky/build/tmp/deploy/images/qemuarm64/xen-image-minimal-qemuarm64.rootfs.ext4,if=virtio,format=raw \
-
+  -device guest-loader,addr=0x45000000,kernel=/home/crs/dom0x/poky/build/tmp/deploy/images/qemuarm64/Image,bootargs="console=hvc0 earlyprintk=xen dom0_mem=512M dom0_max_vcpus=2 loglvl=all guest_loglvl=all loglvl_xen=all" \
+  -drive file=/home/crs/dom0x/poky/build/tmp/deploy/images/qemuarm64/xen-image-minimal-qemuarm64.rootfs.ext4,if=virtio,format=raw \
